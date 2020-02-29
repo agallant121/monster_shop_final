31ce78bcabbfd85beb52175902cf3cb86e16aa6d
Alex Gallant <agallant1201@gmail.com>
2020-02-29 14:34:13 -0700

    Add discounts table

---
+++ db/migrate/20200229212938_create_discounts.rb
@@ -0,0 +1,13 @@
+class CreateDiscounts < ActiveRecord::Migration[5.1]
+  def change
+    create_table :discounts do |t|
+      t.string :name
+      t.string :description
+      t.references :merchant, foreign_key: true
+      t.integer :min_quantity
+      t.integer :max_quantity
+
+      t.timestamps
+    end
+  end
+end
