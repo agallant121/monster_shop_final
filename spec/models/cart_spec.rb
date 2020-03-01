2f2651758f26437f263974d2120fde801f7c1847
Alex Gallant <agallant1201@gmail.com>
2020-03-01 15:08:01 -0700

    Add test for discounted_total helper method

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -3,11 +3,47 @@ require 'rails_helper'
 RSpec.describe Cart do
   describe 'Instance Methods' do
     before :each do
-      @megan = Merchant.create!(name: 'Megans Marmalades', address: '123 Main St', city: 'Denver', state: 'CO', zip: 80218)
-      @brian = Merchant.create!(name: 'Brians Bagels', address: '125 Main St', city: 'Denver', state: 'CO', zip: 80218)
-      @ogre = @megan.items.create!(name: 'Ogre', description: "I'm an Ogre!", price: 20, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 5 )
-      @giant = @megan.items.create!(name: 'Giant', description: "I'm a Giant!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 2 )
-      @hippo = @brian.items.create!(name: 'Hippo', description: "I'm a Hippo!", price: 50, image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw', active: true, inventory: 3 )
+      @megan = Merchant.create!(name: 'Megans Marmalades',
+                                address: '123 Main St',
+                                city: 'Denver',
+                                state: 'CO', zip: 80218)
+
+      @brian = Merchant.create!(name: 'Brians Bagels',
+                                address: '125 Main St',
+                                city: 'Denver',
+                                state: 'CO',
+                                zip: 80218)
+
+      @ogre = @megan.items.create!(name: 'Ogre',
+                                  description: "I'm an Ogre!",
+                                  price: 20,
+                                  image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw',
+                                  active: true,
+                                  inventory: 5 )
+
+      @giant = @megan.items.create!(name: 'Giant',
+                                    description: "I'm a Giant!",
+                                    price: 50,
+                                    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw',
+                                    active: true,
+                                    inventory: 2 )
+
+      @hippo = @brian.items.create!(name: 'Hippo',
+                                    description: "I'm a Hippo!",
+                                    price: 50,
+                                    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw',
+                                    active: true,
+                                    inventory: 3 )
+      @test_item = @megan.items.create!(name: 'Test Item',
+                                    description: "I'm a Test!",
+                                    price: 50,
+                                    image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaLM_vbg2Rh-mZ-B4t-RSU9AmSfEEq_SN9xPP_qrA2I6Ftq_D9Qw',
+                                    active: true,
+                                    inventory: 10 )
+
+      @discount_1 = @megan.discounts.create!(name: "Test_1 Discount", description: "Great bulk discount", min_quantity: 3, max_quantity: 4, percent: 10)
+      @discount_2 = @megan.discounts.create!(name: "Test_2 Discount", description: "Greater bulk discount", min_quantity: 5, max_quantity: 100, percent: 20)
+
       @cart = Cart.new({
         @ogre.id.to_s => 1,
         @giant.id.to_s => 2
@@ -62,6 +98,18 @@ RSpec.describe Cart do
       @cart.less_item(@giant.id.to_s)
 
       expect(@cart.count_of(@giant.id)).to eq(1)
+    end
+
+    it ".discounted_total" do
+      @cart.add_item(@test_item.id.to_s)
+      @cart.add_item(@test_item.id.to_s)
+      @cart.add_item(@test_item.id.to_s)
+      @cart.add_item(@test_item.id.to_s)
+
+      expect(@cart.discounted_total).to eq(300)
+
+      @cart.add_item(@test_item.id.to_s)
+      expect(@cart.discounted_total).to eq(320)
     end
   end
 end
