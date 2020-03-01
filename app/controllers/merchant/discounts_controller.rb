eba3bf655ab94c05a541d7099563507c28bc3ea0
Alex Gallant <agallant1201@gmail.com>
2020-02-29 18:51:42 -0700

    Add new and create actions as well as helper methods for creating a new discount

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -6,4 +6,45 @@ class Merchant::DiscountsController < Merchant::BaseController
   def show
     @discount = Discount.find(params[:discount_id])
   end
+
+  def new
+  end
+
+  def create
+    create_discount
+  end
+
+  private
+
+  def discount_params
+    params.permit(:name, :description, :min_quantity, :max_quantity, :percent)
+  end
+
+  def create_discount
+    merchant = current_user.merchant
+    discount = merchant.discounts.create(discount_params)
+    save_or_not_save_discount
+  end
+
+  def save_or_not_save_discount
+    merchant = current_user.merchant
+    discount = merchant.discounts.create(discount_params)
+    if discount.save
+      saved_discount
+    else
+      discount_not_saved
+    end
+  end
+
+  def saved_discount
+    flash[:notice] = "Your discount has been added."
+    redirect_to "/merchant/discounts"
+  end
+
+  def discount_not_saved
+    merchant = current_user.merchant
+    discount = merchant.discounts.create(discount_params)
+    flash[:notice] = discount.errors.full_messages.to_sentence
+    redirect_to "/merchant/discounts/new"
+  end
 end
